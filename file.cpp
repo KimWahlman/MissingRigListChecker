bac8df0a797271dea23f689b19b06126a48b5fb4
Kim Wahlman <kim.wahlman@gmail.com>
2021-02-15 10:55:25 +0100

    Update file.cpp
    Added support to check both tent and containers.

--- file.cpp
+++ file.cpp
@@ -8,7 +8,7 @@
 void readFile(std::string filename, std::vector<std::string>& v) {
 	std::ifstream inFile(filename);
 	std::string line = "";
-	
+
 	while(std::getline(inFile, line)) {
 		v.push_back(line);
 	}
@@ -26,14 +26,31 @@ std::string printDateTime() {
 void run() {
 	std::vector<std::string> f1;
 	std::vector<std::string> f2;
-	
-	readFile("trueList.txt", f1);
-	readFile("listToCheck.txt", f2);
-	
+
+	std::cout << "Check:\n1. Tent\n2. Container\n\n>> ";
+	unsigned int option = 0;
+	std::cin >> option;
+
+	if(option == 1)
+    {
+        readFile("tent_trueList.txt", f1);
+        readFile("tent_listToCheck.txt", f2);
+    } else {
+        readFile("container_trueList.txt", f1);
+        readFile("container_listToCheck.txt", f2);
+    }
+
 	bool exists = false;
-	
-	std::ofstream fileToSave("missingRigs " + printDateTime() + ".txt");
-	
+
+	std::string filename;
+
+	if(option == 1)
+        filename = "tent_missingRigs " + printDateTime() + ".txt";
+    else
+        filename = "container_missingRigs " + printDateTime() + ".txt";
+
+    std::ofstream fileToSave(filename);
+
 	if(fileToSave.is_open()) {
 		fileToSave << printDateTime() << std::endl;
 		for(unsigned int i = 0; i < f1.size(); i++) {
@@ -41,7 +58,7 @@ void run() {
 			for(unsigned int j = 0; j < f2.size(); j++) {
 				if(f1[i] == f2[j]) { exists = true; }
 			}
-			
+
 			if(!exists) {
 				std::string str = f1[i] + " is missing.";
 				fileToSave << str << std::endl;
